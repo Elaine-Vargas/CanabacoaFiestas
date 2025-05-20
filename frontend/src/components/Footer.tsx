1a0aa28319dbe07aff4ee7ad521e94dd68667952
ElaineVargas <elainevargasreyes@gmail.com>
2025-05-19 22:25:59 -0400

    fix & add: footer and scss
    Footer structure and main scss

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,24 +1,46 @@
-import Logo from "../assets/logoVariants/OVALO-CF(titulo blanco).svg";
 import "../styles/Footer.scss";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import MailIcon from '@mui/icons-material/MailOutlineRounded';
+
 const Footer = () => {
   return (
 <>
 <footer>
     <div className='footer'>
-        <div className="footerImgContainer">
-            <img className='footerLogo' src={Logo} alt="CF Logo"/>
+        <div className="footerMap">
+            <h3>Nos encuentras en...</h3>
+            <h5>Santiago de los Caballeros</h5>
+            <iframe className="mapFrame" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d2390.0520887179878!2d-70.65605755554813!3d19.424924246701103!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sdo!4v1747679609801!5m2!1ses!2sdo" loading="lazy"></iframe>
         </div>
-        <div className='footerContainer'>
+
+        <div className="footerComments">
+        <p>comentarios</p>
+        </div>
+
+ <div className='footerInfo'>
         <div className='footerInfo'>
-            <h3>Información</h3>
-            <p>Dirección: Calle 1, Santiago de los Caballeros, República Dominicana</p>
-            <p>Teléfono: +0 (000) 000-0000</p>
-            <p>Email:</p>
+            <div className="ContactItem">
+                <InstagramIcon className="contactIcons" sx={{fontSize:'3vw'}}/>
+                <a href="https://www.instagram.com/canabacoa_fiestas?igsh=MTB4Ymg4N3o4NXI3dw==" target="_blank">@canabacoa_fiestas</a>
             </div>
+            
+            <div className="ContactItem">
+                <a className='contactPhones' href="tel:+18297700926">829-770-0926</a> 
+                <WhatsAppIcon className="contactIcons"  sx={{fontSize:'3vw'}}/>
+                <a className='contactPhones' href="tel:+18093918940">809-391-8940</a>
             </div>
-            <div className='footerMap'>
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1685.953302039527!2d-70.65642057213539!3d19.424195904234644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sdo!4v1747414186066!5m2!1ses!2sdo"  loading="lazy"></iframe>
-</div></div>
+
+            <div className="ContactItem">
+                <MailIcon className="contactIcons"  sx={{fontSize:'3vw'}}/>
+                <a className='contactMail' href="mailto:soporte@canabacoafiesta.online">soporte@canabacoafiesta.online</a>
+            </div>
+    </div>
+    </div>
+
+        {/* //footer v1
+ */}
+    </div>
 
 </footer>
 </>
