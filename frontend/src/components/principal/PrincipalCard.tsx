253dcfc6258f00a3acafdedd9bbfa65a5d753155
Abigail2197 <contrerasabigail215@gmail.com>
2025-06-02 14:03:32 -0400

    Fix: Principal
    fix button direction

--- frontend/src/components/principal/PrincipalCard.tsx
+++ frontend/src/components/principal/PrincipalCard.tsx
@@ -39,7 +39,7 @@ const PrincipalCard = React.memo(() => {
       </p>
     </div>
   </div>
-        <button className="ButtonFindMore"><Link to='/Servicios'>Descubre más</Link></button>
+        <button className="ButtonFindMore"><Link to='/Principal/Servicios'>Descubre más</Link></button>
 
 </div>
 
